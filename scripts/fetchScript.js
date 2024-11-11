694b94191dd0942e0b4b6c3386467c2199344cd9
sean <sean.li@vectorindex.cloud>
2024-11-11 11:09:47 -0800

    Update remote URL in fetchScript.js

--- scripts/fetchScript.js
+++ scripts/fetchScript.js
@@ -1,5 +1,5 @@
 async function fetchScript(script) {
-    const url = `https://raw.githubusercontent.com/yuxiaoli/scripts/refs/heads/main/src/js/${script}.js`
+    const url = `https://raw.githubusercontent.com/Vector-Index/editor/refs/heads/main/scripts/${script}.js`
     const text = await window.utils.api.get(url)
     content.value = text
 }
