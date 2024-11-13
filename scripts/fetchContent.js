f3cdaf92eb504613d3e36be6316cfff14ede0569
sean <sean.li@vectorindex.cloud>
2024-11-12 16:58:06 -0800

    Fix typo: window.utils -> window.editor

--- scripts/fetchContent.js
+++ scripts/fetchContent.js
@@ -1,5 +1,5 @@
 async function fetchContent(url) {
-    const text = await window.utils.api.get(url)
+    const text = await window.editor.api.get(url)
     content.value = text
 }
 fetchContent(args.url)
--- scripts/fetchScript.js
+++ scripts/fetchScript.js
@@ -1,6 +1,6 @@
 async function fetchScript(script) {
     const url = `https://raw.githubusercontent.com/Vector-Index/editor/refs/heads/main/scripts/${script}.js`
-    const text = await window.utils.api.get(url)
+    const text = await window.editor.api.get(url)
     content.value = text
 }
 fetchScript(args.script)
